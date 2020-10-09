5ba16408da23094adb068d73274e5c9462e0da86
turbobo <735567435@qq.com>
2020-10-09 19:10:48 +0800

    eureka单机--->集群

--- cloud-eureka-server7002/src/main/java/com/demo/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/demo/springcloud/EurekaMain7002.java
@@ -2,19 +2,17 @@ package com.demo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author Jusven
- * @Date 2020/9/10 19:10
- * @Version 1.0
- *
- *  微服务提供者
+ * @Date 2020/10/9 18:53
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer  //标识为sEureka服务端
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
+
     }
 }
